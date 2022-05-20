f8ce2261dcb81aafc7ab4222ffdac593262288df
b4rt01012 <b4rt01012@gmail.com>
2022-05-20 12:43:49 -0500

    H1: css - js

--- js/add.js
+++ js/add.js
@@ -2,5 +2,9 @@ var i = 1;
 var j = 2;
 
 function saludo3(){
-	alert('saludo');
+	alert('saludos amigo');
+}
+
+function alerta(){
+	alert('heyyyyy');
 }
--- js/update.js
+++ js/update.js
@@ -2,5 +2,9 @@ var i = 1;
 var j = 2;
 
 function saludo2(){
-	alert('saludo');
+	alert('saludo bro');
+}
+
+function saludo5(){
+	alert('hi bro!!');
 }
